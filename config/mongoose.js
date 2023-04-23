eef2e31575b0989f1ad6ae48fa4539976edd4c47
rkchy23 <101350205+rkchy23@users.noreply.github.com>
2023-04-23 23:35:02 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Define an async function to establish a connection to a MongoDB database cluster.
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+    await mongoose.connect('mongodb+srv://debajyotishit3:Deba16@cluster0.fxnuinl.mongodb.net/?retryWrites=true&w=majority');
     console.log("connection Successfull !! ");
 }
 
